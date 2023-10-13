2d05416349b093b0772e2864382f9339c184ae92
redairforce <redairforce@gmail.com>
2023-10-13 00:59:58 -0500

    Update install.sh

--- extras/install.sh
+++ extras/install.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 
-ORIGIN_REPO="https://github.com/mrworf/plexupdate"  # Default GitHub repository
-FULL_PATH="/opt/plexupdate"  # Default installation directory
+ORIGIN_REPO="https://github.com/${GIT_OWNER:-mrworf}/plexupdate"
+FULL_PATH="/opt/plexupdate"
+CONFIGFILE="/etc/plexupdate.conf"
+CONFIGCRON="/etc/plexupdate.cron.conf"
+CRONWRAPPER="/etc/cron.daily/plexupdate"
+VERBOSE=yes
 
-# Function to install plexupdate
-# Function to install or update plexupdate
+# Default options
+AUTOINSTALL=yes
+AUTOUPDATE=yes
+PUBLIC=
+DISTRO_INSTALL="apt install"  # Default distro installation command
+
 install_plexupdate() {
-    if [ ! -d "${FULL_PATH}/.git" ]; then
-        echo "Cloning plexupdate repository into '$FULL_PATH'... "
-        git clone --branch "master" "$ORIGIN_REPO" "$FULL_PATH" || exit 1
-        echo "Clone completed."
+    if [ -d "${FULL_PATH}" ]; then
+        # If directory exists and is not empty, clear its contents
+        if [ -n "$(ls -A ${FULL_PATH})" ]; then
+            echo "Clearing contents of '$FULL_PATH'... "
+            rm -rf ${FULL_PATH}/*
+            echo "Cleared contents."
+        fi
     else
-        echo "Plexupdate repository already exists in '$FULL_PATH', updating... "
-        cd "$FULL_PATH" || exit 1
-        git fetch origin
-        git reset --hard origin/master || exit 1
-        echo "Update completed."
-        cd - || exit 1
+        echo "Creating directory '$FULL_PATH'... "
+        mkdir -p ${FULL_PATH} || exit 1
+        echo "Created directory."
     fi
+
+    echo "Cloning plexupdate repository into '$FULL_PATH'... "
+    git clone --branch "${BRANCHNAME:-master}" "$ORIGIN_REPO" "$FULL_PATH" || exit 1
+    echo "Clone completed."
+}
+
+configure_plexupdate() {
+    # Configuration logic here (assuming default values for now)
+}
+
+configure_cron() {
+    # Configuration logic here (assuming default values for now)
 }
 
 # Installation logic
 install_plexupdate
+configure_plexupdate
+configure_cron
 
 # Run plexupdate with the configured settings
 echo "Running plexupdate with the configured settings... "
-"$FULL_PATH/plexupdate.sh" --config "/etc/plexupdate.conf"
+"$FULL_PATH/plexupdate.sh" --config "$CONFIGFILE"
